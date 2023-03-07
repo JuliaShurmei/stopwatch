467922d9193787cfcdfb5f4748dd57dc5cbac327
JuliaShurmei <ivanovaulia428@gmail.com>
2023-03-07 12:00:00 +0300

    Fixed styles

--- sctipt.js
+++ sctipt.js
@@ -3,3 +3,9 @@ let stopBtn = document.getElementById('stopBtn');
 let startBtn = document.getElementById('startBtn');
 let resetBtn = document.getElementById('resetBtn');
 
+let msec = 0o0;
+let secs = 0o0;
+let mins = 0o0;
+
+let timerId = null;
+
